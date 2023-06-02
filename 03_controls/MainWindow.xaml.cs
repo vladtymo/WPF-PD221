97233ac1e54a58dd7afd81ab354763e6b75c57ef
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-06-02 16:30:19 +0300

    Controls example.

---
+++ 03_controls/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (datePicker.SelectedDate != null)
+                this.Title = datePicker.SelectedDate.Value.ToLongDateString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (calendar.SelectedDate != null)
+                MessageBox.Show($"From: {calendar.SelectedDates.First()}\nTo: {calendar.SelectedDates.Last()}", "Selected Date Range");
+        }
+    }
+}
