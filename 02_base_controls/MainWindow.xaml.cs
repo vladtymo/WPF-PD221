0dc25dcdc2e523822e65f56b792c68a2b89edfda
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-06-02 16:10:19 +0300

    Base controls example.

---
+++ 02_base_controls/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace _02_base_controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer _timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Clicked!!!", "Event", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = $"{DateTime.Now:mm:ss:FFF}";
+        }
+    }
+}
