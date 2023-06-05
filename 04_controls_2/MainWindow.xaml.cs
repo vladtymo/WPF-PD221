39d7cc4a8799b802f2ba6ac28596da6fbb68ef4d
vladtymo <tymo.vlad@gmail.com>
2023-06-05 16:08:36 +0300

    Range Controls. Status Bar.

---
+++ 04_controls_2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace _04_controls_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (progressBar.Value == progressBar.Maximum)
+                progressBar.Value = progressBar.Minimum;
+            else
+                progressBar.Value++;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) => timer.Start();
+        private void Button_Click_1(object sender, RoutedEventArgs e) => timer.Stop();
+
+        // slider Value Changes event
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.Opacity = slider.Value / 100;
+        }
+
+        // enable moving Window if Window Style = None
+        //protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        //{
+        //    base.OnMouseLeftButtonDown(e);
+        //    // Begin dragging the window
+        //    this.DragMove();
+        //}
+    }
+}
