316c9c3e9910d94cff35dc50b7a7b68f1f10ed7a
vladtymo <tymo.vlad@gmail.com>
2023-06-07 16:23:51 +0300

    Bindings. Data Context example.

---
+++ 07_bindings/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _07_bindings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel viewModel = new("Intro to ViewModel");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // allow window controls to binding to User object
+            this.DataContext = viewModel; // View -> View Model
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(viewModel.CurrentUser.ToString());
+        }
+    }
+
+    public class ViewModel
+    {
+        public User CurrentUser { get; set; }
+        public string Title { get; set; }
+        public Color Color { get; set; }
+
+        public ViewModel(string title)
+        {
+            CurrentUser = new() { Username = "tymo@gmail.com", Score = 33 };
+            this.Title = title;
+            Color = Colors.DarkGray;
+        }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+
+        public override string ToString()
+        {
+            return $"User {Username} has socre of {Score}";
+        }
+    }
+}
