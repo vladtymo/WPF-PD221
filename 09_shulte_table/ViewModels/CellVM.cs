7b954ae6090d87ffea2df2d25311a92315a7209d
vladtymo <tymo.vlad@gmail.com>
2023-06-16 17:49:25 +0300

    Implement Shulte Table base functionality using MVVM.

---
+++ 09_shulte_table/ViewModels/CellVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09_shulte_table.ViewModels
+{
+    public enum CellColor
+    {
+        White, Green, Purple, Yellow, Blue, Red
+    };
+    public enum CellState
+    {
+        Current, Closed, Next
+    }
+
+    public class CellVM
+    {
+        private static Random rnd = new();
+        // command
+
+        // -------- Binding Properties --------
+        public int Number { get; }
+        public CellColor Color { get; set; }
+        public CellState State { get; set; }
+        public bool IsWrong { get; set; }
+
+        public CellVM(int number) : this(number, GetRandomColor()) { }
+        public CellVM(int number, CellColor color)
+        {
+            Number = number;
+            Color = color;
+
+            IsWrong = true;
+        }
+        private static CellColor GetRandomColor()
+        {
+            return (CellColor)rnd.Next(Enum.GetValues(typeof(CellColor)).Length);
+        }
+    }
+}
---
+++ 09_shulte_table/ViewModels/TableVM.cs
@@ -0,0 +1,41 @@
+﻿using _09_shulte_table.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _09_shulte_table.ViewModels
+{
+    public class TableVM
+    {
+        private IList<CellVM> cells = new List<CellVM>();
+
+        // -------- Binding Properties --------
+        public IEnumerable<CellVM> Cells => cells; 
+        public int Size { get; }
+
+        public const int EYE_NUMBER = 0;
+        public int FirstNumber { get; } = 1;
+        public int LastNumber { get; }
+
+        public TableVM(int size)
+        {
+            Size = size;
+            LastNumber = Size * Size - 1; // due to one eye-cell on the center
+
+            GenerateCells();
+        }
+
+        private void GenerateCells()
+        {
+            for (int i = FirstNumber; i <= LastNumber; ++i)
+            {
+                cells.Add(new CellVM(i));
+            }
+            this.cells.Shuffle();
+
+            cells.Insert(cells.Count / 2, new CellVM(EYE_NUMBER, CellColor.White));
+        }
+    }
+}
